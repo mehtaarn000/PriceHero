07dc9cec7e104c9f8bc88089a8ba2789a3a78185
mehtaarn000 <66114536+mehtaarn000@users.noreply.github.com>
2020-09-08 20:02:44 -0500

    Add files via upload

---
+++ websites/acer.py
@@ -0,0 +1,24 @@
+from selectorlib import Extractor
+from requests import get
+
+extractor = Extractor.from_yaml_file('selectors.yml')
+
+def acer(acer_url):
+    headers = {
+        'authority': 'www.acer.com',
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'none',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-dest': 'document',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8'}
+
+    website = get(acer_url, headers=headers)
+    productdata = extractor.extract(website.text)
+    print(productdata)
+
+acer('https://store.acer.com/en-us/swift-7-laptop-sf714-52t-75r6')
