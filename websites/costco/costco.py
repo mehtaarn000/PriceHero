a75808ddf01e688293f0aff9443207987733f1d3
mehtaarn000 <66114536+mehtaarn000@users.noreply.github.com>
2020-09-12 18:10:03 -0500

    change print to return

--- websites/costco/costco.py
+++ websites/costco/costco.py
@@ -8,8 +8,6 @@ def costco(costco_url):
     website
     costco_price = driver.find_element_by_class_name('value')
     costco_name = driver.find_element_by_css_selector('#product-details > div.row.visible-lg.visible-xl > div > div.product-h1-container-v2.visible-lg-block.visible-xl-block > h1')
-    print('$', costco_price.text)
-    print(costco_name.text)
+    productdata = {"price":"", "name":""}
+    return productdata
     driver.quit()
-
-costco('https://www.costco.com/apple-mac-mini---intel-core-i3---8gb-memory---256gb-ssd.product.100531970.html')
