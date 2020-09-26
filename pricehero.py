e10037b3fd7268ade4b6e575ca6f93b61e973126
Arnav Mehta <66114536+mehtaarn000@users.noreply.github.com>
2020-09-26 01:14:10 -0500

    add support for adafruit + improvements

--- pricehero.py
+++ pricehero.py
@@ -4,27 +4,31 @@ from websites.dell import dell as d
 from websites.michaels import michaels as e
 from websites.acer import acer as f
 from websites.walmart import walmart as g
+from websites.adafruit import adafruit as h
 
 def amazon(self):
-    amazon_product = a(self)
+    amazon_product = a.amazon(self)
     return amazon_product
 
 def bestbuy(self):
-    bestbuy_product = b(self)
+    bestbuy_product = b.bestbuy(self)
     return bestbuy_product
 
 def dell(dell_url):
-    dell_product = d(dell_url)
+    dell_product = d.dell(dell_url)
     return dell_product
 
 def michaels(michaels_url):
-    michaels_product = e(michaels_url)
+    michaels_product = e.michaels(michaels_url)
     return michaels_product
 
 def acer(acer_url):
-    acer_product = f(acer_url)
+    acer_product = f.acer(acer_url)
     return acer_product
 
 def walmart(walmart_url):
-    walmart_product = g(walmart_url)
+    walmart_product = g.walmart(walmart_url)
     return walmart_product
+
+def adafruit(adafruit_url):
+    adafruit_product = h.adafruit(adafruit_url)
