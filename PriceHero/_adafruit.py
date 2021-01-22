2e79b7767f3723b641478a7ff95d9bcc017f9bbf
arnavmehta1977@gmail.com <66114536+mehtaarn000@users.noreply.github.com>
2021-01-22 02:55:33 -0600

    Add headers to adafruit and github shop

--- PriceHero/_adafruit.py
+++ PriceHero/_adafruit.py
@@ -12,6 +12,20 @@ def _adafruit(adafruit_url):
         xpath: null
         type: Text
     """)
+
+    headers = {
+        'authority': 'www.acer.com',
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'none',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-dest': 'document',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8'}
+        
     website = get(adafruit_url)
     productdata = extractor.extract(website.text)
     return productdata
