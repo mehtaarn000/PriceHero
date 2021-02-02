50d948b3f339791e83d406eb9375219afaf85a8f
arnavmehta1977@gmail.com <66114536+mehtaarn000@users.noreply.github.com>
2021-02-01 22:10:40 -0600

    Add types to __init__.py

--- PriceHero/__init__.py
+++ PriceHero/__init__.py
@@ -11,50 +11,50 @@ from . import _github_shop
 from . import _macys
 from . import _chess_shop
 
-def amazon(self):
-    amazon_product = _amazon._amazon(self)
+def amazon(amazon_url : str) -> dict:
+    amazon_product = _amazon._amazon(amazon_url)
     return amazon_product
 
-def bestbuy(self):
-    bestbuy_product = _bestbuy._bestbuy(self)
+def bestbuy(bestbuy_url : str) -> dict:
+    bestbuy_product = _bestbuy._bestbuy(bestbuy_url)
     return bestbuy_product
 
-def dell(dell_url):
+def dell(dell_url : str) -> dict:
     dell_product = _dell._dell(dell_url)
     return dell_product
 
-def michaels(michaels_url):
+def michaels(michaels_url : str) -> dict:
     michaels_product = _michaels._michaels(michaels_url)
     return michaels_product
 
-def acer(acer_url):
+def acer(acer_url : str) -> dict:
     acer_product = _acer._acer(acer_url)
     return acer_product
 
-def walmart(walmart_url):
+def walmart(walmart_url : str) -> dict:
     walmart_product = _walmart._walmart(walmart_url)
     return walmart_product
 
-def adafruit(adafruit_url):
+def adafruit(adafruit_url : str) -> dict:
     adafruit_product = _adafruit._adafruit(adafruit_url)
     return adafruit_product
 
-def xbox(xbox_url):
+def xbox(xbox_url : str) -> dict:
     xbox_product = _xbox._xbox(xbox_url)
     return xbox_product
 
-def joanns(joanns_url):
+def joanns(joanns_url : str) -> dict:
     joanns_product = _joanns._joanns(joanns_url)
     return joanns_product
 
-def github_shop(github_shop_url):
+def github_shop(github_shop_url : str) -> dict:
     github_shop_product = _github_shop._github_shop(github_shop_url)
     return github_shop_product
 
-def macys(macys_url):
+def macys(macys_url : str) -> dict:
     macys_product = _macys._macys(macys_url)
     return macys_product
 
-def chess_shop(chess_shop_url):
+def chess_shop(chess_shop_url : str) -> dict:
     chess_shop_product = _chess_shop._chess_shop(chess_shop_url)
     return chess_shop_product
