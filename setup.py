70c650d97c931e5a02da8302ace42bb0134a1cdc
arnavmehta1977@gmail.com <66114536+mehtaarn000@users.noreply.github.com>
2021-02-23 14:23:44 -0600

    Remove setup.py from .gitignore (for GitHub Actions)

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PriceHero-mehtaarn000",
+    version="0.3.9",
+    author="mehtaarn000",
+    description="A python module that will allow users to get product prices information from a multitude of different websites. ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mehtaarn000/PriceHero",
+    packages=['PriceHero'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
